236b31c97dbe35dcfeb7fa9b030b65ab5ac2637f
sankita15 <ankitacs0018@gmail.com>
2019-03-11 12:27:29 +0530

    Remove common code from page.js

--- frontend/src/Page.js
+++ frontend/src/Page.js
@@ -6,22 +6,19 @@ import Products from './page/Products';
 import ItemDescription from './page/ItemDescription';
 import Cart from './page/Cart';
 import OrderPage from './page/OrderPage';
+import RegistrationPage from './page/RegistrationPage';
 
 
 // const ItemDescriptionWrapper = ({ match: { params: { id } } }, props) => <ItemDescription id={id} {...props}/>;
 
-
 const Page = props => (
     <Router>
         <Switch>
-            <Route exact path="/">
+            <Route exact path={['/', '/products']}>
                 <Products {...props} />
             </Route>
             <Route exact path="/login">
                 <LoginPage {...props} />
-            </Route>
-            <Route exact path="/products">
-                <Products {...props} />
             </Route>
             <Route exact path="/carts">
                 <Cart {...props} />
